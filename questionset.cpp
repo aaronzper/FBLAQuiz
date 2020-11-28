087064e25e2132d02ab840d3d6eb358ea586a951
skipper1931 <lordskipper1931@gmail.com>
2020-11-28 13:33:04 -0500

    Reformatted questionset.cpp
    Removed outdated comment and fixed indents

--- questionset.cpp
+++ questionset.cpp
@@ -4,8 +4,6 @@
 #include <vector>
 #include <iostream>
 #include "question.h"
-#include <stdlib.h>
-#include <variant>
 
 QuestionSet::QuestionSet(QString path)
 {
@@ -19,22 +17,21 @@ QuestionSet::QuestionSet(QString path)
     for(QString line = txt.readLine(); !line.isNull(); line = txt.readLine()) {
         int type = line[0].digitValue();
         line.remove(0,1); // remove first char (the question type) since the question constructors dont need that info
-        QuestionContainer q;
 
-        // Heap-allocate pointers to each question for the array, so I can cast them from the base Question class to the type-specific question classes (TrueFalseQuestion, etc)
+        QuestionContainer q;
         switch(type) {
-            case 0:     // True/false
-                q = TrueFalseQuestion(line);
-                break;
-            case 1:     // Multi-choice
-                q = MultiChoiceQuestion(line);
-                break;
-            case 2:     // Short answer
-                q = ShortAnswerQuestion(line);
-                break;
-            default:    // Invalid type (including -1, for when its not even an int)
-                throw std::runtime_error("Malformed question file");
-                break;
+        case 0:     // True/false
+            q = TrueFalseQuestion(line);
+            break;
+        case 1:     // Multi-choice
+            q = MultiChoiceQuestion(line);
+            break;
+        case 2:     // Short answer
+            q = ShortAnswerQuestion(line);
+            break;
+        default:    // Invalid type (including -1, for when its not even an int)
+            throw std::runtime_error("Malformed question file");
+            break;
         }
 
         questions.push_back(q);
