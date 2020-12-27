0871e72e69751dbeafd02fec0524fd7d5ef13766
skipper1931 <lordskipper1931@gmail.com>
2020-12-27 14:31:13 -0500

    Added list of questions to printed report

--- src/questionframe_print.cpp
+++ src/questionframe_print.cpp
@@ -1,17 +1,28 @@
 #include "questionframes.h"
 
+#define INIT_HTML_OUTPUT()												\
+	QString htmlOutput = "<h2>" + questionLabel->text() + "</h2>\n";	\
+
 QString TrueFalseFrame::generateReport() {
-	return "<h2>foo</h2>\n";
+	INIT_HTML_OUTPUT();
+
+	return htmlOutput;
 }
 
 QString MultiChoiceFrame::generateReport() {
-	return "<h2>foo</h2>\n";
+	INIT_HTML_OUTPUT();
+
+	return htmlOutput;
 }
 
 QString ShortAnswerFrame::generateReport() {
-	return "<h2>foo</h2>\n";
+	INIT_HTML_OUTPUT();
+
+	return htmlOutput;
 }
 
 QString MultiAnswerFrame::generateReport() {
-	return "<h2>foo</h2>\n";
+	INIT_HTML_OUTPUT();
+
+	return htmlOutput;
 }
